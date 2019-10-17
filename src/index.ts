e284f26b20e2d3a6d0b77e1b448f105e5f21c286
uzimike <kotlyamich@ecs.vuw.ac.nz>
2019-10-17 13:04:57 +1300

    preperation for testing

--- src/index.ts
+++ src/index.ts
@@ -3,33 +3,33 @@ import Rover from './Rover';
 import * as Operation from './Operation';
 import { Direction, Move } from './constants';
 
-let plateau: Plateau = new Plateau(5, 5, [
-  new Rover(1, 2, Direction.N)
-]);
+// let plateau: Plateau = new Plateau(5, 5, [
+//   new Rover(1, 2, Direction.N)
+// ]);
 
-Operation.moveRover(plateau, 0, Move.L);
-Operation.moveRover(plateau, 0, Move.M);
-Operation.moveRover(plateau, 0, Move.L);
-Operation.moveRover(plateau, 0, Move.M);
-Operation.moveRover(plateau, 0, Move.L);
-Operation.moveRover(plateau, 0, Move.M);
-Operation.moveRover(plateau, 0, Move.L);
-Operation.moveRover(plateau, 0, Move.M);
-Operation.moveRover(plateau, 0, Move.M);
+// Operation.moveRover(plateau, 0, Move.L);
+// Operation.moveRover(plateau, 0, Move.M);
+// Operation.moveRover(plateau, 0, Move.L);
+// Operation.moveRover(plateau, 0, Move.M);
+// Operation.moveRover(plateau, 0, Move.L);
+// Operation.moveRover(plateau, 0, Move.M);
+// Operation.moveRover(plateau, 0, Move.L);
+// Operation.moveRover(plateau, 0, Move.M);
+// Operation.moveRover(plateau, 0, Move.M);
 
-Operation.addRover(plateau, new Rover(3, 3, Direction.E));
+// Operation.addRover(plateau, new Rover(3, 3, Direction.E));
 
-Operation.moveRover(plateau, 1, Move.M);
-Operation.moveRover(plateau, 1, Move.M);
-Operation.moveRover(plateau, 1, Move.R);
-Operation.moveRover(plateau, 1, Move.M);
-Operation.moveRover(plateau, 1, Move.M);
-Operation.moveRover(plateau, 1, Move.R);
-Operation.moveRover(plateau, 1, Move.M);
-Operation.moveRover(plateau, 1, Move.R);
-Operation.moveRover(plateau, 1, Move.R);
-Operation.moveRover(plateau, 1, Move.M);
+// Operation.moveRover(plateau, 1, Move.M);
+// Operation.moveRover(plateau, 1, Move.M);
+// Operation.moveRover(plateau, 1, Move.R);
+// Operation.moveRover(plateau, 1, Move.M);
+// Operation.moveRover(plateau, 1, Move.M);
+// Operation.moveRover(plateau, 1, Move.R);
+// Operation.moveRover(plateau, 1, Move.M);
+// Operation.moveRover(plateau, 1, Move.R);
+// Operation.moveRover(plateau, 1, Move.R);
+// Operation.moveRover(plateau, 1, Move.M);
 
-for (var i = 0; i < plateau.rovers.length; i++) {
-  console.log(plateau.rovers[i].x + ' ' + plateau.rovers[i].y + ' ' + plateau.rovers[i].dir);
-}
+// for (var i = 0; i < plateau.rovers.length; i++) {
+//   console.log(plateau.rovers[i].x + ' ' + plateau.rovers[i].y + ' ' + plateau.rovers[i].dir);
+// }
